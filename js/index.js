9f5d690ff03b727b42ffd316709098ad145ec110
Hussien Sulyman <103793784+hussiensulyman@users.noreply.github.com>
2024-07-14 11:48:35 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+});
